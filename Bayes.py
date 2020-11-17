0afd7db596affdf9e32c4bc422d523bb979ef74c
XiaoHuoZZ <314297421@qq.com>
2020-11-18 00:03:52 +0800

    update

--- Bayes.py
+++ Bayes.py
@@ -6,8 +6,9 @@ import pandas as pd
 import csv
 import random
 import re
-from multiprocessing import Pool,Manager,Lock,Array
+from multiprocessing import Pool,Manager,Lock,Array,Process
 import time as tu
+from functools import partial
 
 
 
@@ -37,9 +38,9 @@ def partition():
                 writer1.writerow(row) 
             else:
                 writer2.writerow(row) 
-        f.close
-        tr.close
-        te.close
+        f.close()
+        tr.close()
+        te.close()
     print('划分完毕')
 
 
@@ -57,13 +58,38 @@ def handle_fenci(data,m,n,stop_list,cat,dic):
     print('done:'+str(n))
     return dic
     
-def handle_transform():
-    return
+def handle_tf(cat,dic):
+    pool = Pool(2)
+    bow = np.zeros(len(dic[cat]))
+    with open('temp/'+ cat +'.csv',encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            doc = row[0].split(',')
+            res = pool.map(partial(handle_tf1,l=dic[cat]),doc)
+            for i in res:
+                if i>=0:
+                    bow[i] = bow[i] + 1
+            # for d in doc:
+            #     i = dic[cat].index(d) #查找该词在词典中的位置
+            #     bow[i] = bow[i] + 1
+        pool.close()
+        f.close()
+    print('done:'+cat)
+    return bow
+
+def handle_tf1(d,l):
+    if d == '':
+        print('error')
+        return -1
+    i = l.index(d) #查找该词在词典中的位置
+    return i
+
 
 def pretreatment():
     t_start=tu.time()
     res_list=[]
     pool = Pool(12)
+    manager = Manager()
     stop_list=[]
     category = []
     #加载停用词表
@@ -71,52 +97,52 @@ def pretreatment():
         for line in f:
             stop_list.append(line.strip('\n'))
         f.close()
+    #加载数据
     with open('./data/train.csv', 'r',encoding='utf-8') as f:
-        manager = Manager()
         reader = csv.reader(f)
         data = manager.list()
-        
         for i,row in enumerate(reader):
             if i != 0:
                 data.append(row[2])
                 category.append(row[0])
                 if row[0] == 'category':
                     print(row[2])
-        
-        cat_list = list(set(category))
-        dic = {}
-        for cat in cat_list:
-            dic[cat] = []
-        
-        print('load')
-        size = len(data)
-        ratio = 10000
-        t = size//ratio
-        offset = size-t*ratio
-        for i in range(t):
-            res = pool.apply_async(func=handle_fenci, args=(data,i*ratio,(i+1)*ratio,stop_list,category,dic,))
-            res_list.append(res)
-        res = pool.apply_async(func=handle_fenci, args=(data,t*ratio,t*ratio+offset,stop_list,category,dic,))
-        res_list.append(res)
-        f.close
+        f.close()
+    print('load')
 
-    
+    #获得种类列表
+    cat_list = list(set(category))
+    dic = {}
+    for cat in cat_list:
+        dic[cat] = []
+        
+   
+    #任务分解
+    size = len(data)
+    ratio = 10000   #每个进程处理文档数
+    t = size//ratio
+    offset = size-t*ratio
+    for i in range(t):
+        res = pool.apply_async(func=handle_fenci, args=(data,i*ratio,(i+1)*ratio,stop_list,category,dic,))
+        res_list.append(res)
+    res = pool.apply_async(func=handle_fenci, args=(data,t*ratio,t*ratio+offset,stop_list,category,dic,))
+    res_list.append(res)
     pool.close()
     pool.join()
 
-    #去重
-    print('\n save')
+    print('\n start save')
     l_start = tu.time()
 
     dic = {}
     for cat in cat_list:
         dic[cat] = []
     
+    #取分词结果并保存到dic
     for res in res_list:
         temp = res.get()
         for cat in cat_list:
             nL = dic[cat] + temp[cat]
-            dic[cat] = list(set(nL))
+            dic[cat] = list(set(nL))  #去重
             
     #保存处理结果
     for i in range(size):
@@ -125,16 +151,15 @@ def pretreatment():
         with open('./temp/'+ cat +'.csv','a',encoding='utf-8',newline='') as f:
             writer = csv.writer(f)
             writer.writerow([doc])
-            f.close
+            f.close()
 
     with open('words_list','w',encoding='utf-8') as f:
         f.write(str(dic))
-        f.close
+        f.close()
 
     l_end = tu.time()
     l_time = l_end-l_start
     print ('the save time is :%s' %l_time)
-
 
 
     t_end=tu.time()
@@ -146,33 +171,49 @@ def pretreatment():
     print('complete')
     
 def transform():
-    print('start')
+    start = tu.time()
+    res_list=[]
+    manager = Manager()
+    #加载字典
     with open('words_list',encoding='utf-8') as f:
-        dic = eval(f.read())
+        dic = manager.dict(eval(f.read()))
         f.close()
     
+    #种类列表
     cat_list = list(dic.keys())
 
-    start = tu.time()
+    #词袋模型
+    # bow ={}
+    # for cat in cat_list:
+    #     szie  = len(dic[cat])
+    #     bow[cat] = np.zeros(szie)
 
-    bow = {}
-    for cat in cat_list:
-        szie  = len(dic[cat])
-        bow[cat] = np.zeros(szie)
+    print('start transform')
 
-    manager = Manager()
-
-    m_dic = manager.dict(dic)
-
-    for cat in cat_list:
-        with open('temp/'+ cat +'.csv',encoding='utf-8') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                doc = row[0].split(',')
-                for d in doc:
-                    i = dic[cat].index(d) #查找该词在词典中的位置
-                    bow[cat][i] = bow[cat][i] + 1
     
+    for cat in cat_list:
+        # res = pool.apply_async(func=handle_tf, args=(cat,dic,))
+        # res_list.append(res)
+        process = Process(target=handle_tf, args=(cat,dic))
+        process.start()
+        res_list.append(process)
+    for res in res_list:
+        res.join()
+
+
+    
+    # for cat in cat_list:
+    #     with open('temp/'+ cat +'.csv',encoding='utf-8') as f:
+    #         reader = csv.reader(f)
+    #         for row in reader:
+    #             doc = row[0].split(',')
+    #             res = pool.map(partial(handle_tf1,l=dic[cat]),doc)
+    #             print('done')
+            # for d in doc:
+            #     i = dic[cat].index(d) #查找该词在词典中的位置
+            #     bow[i] = bow[i] + 1
+            # f.close()
+
     end = tu.time()
 
     print(end-start)
