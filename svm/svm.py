f29dc3a02561888a5f7f5bfb99ef6b4c64ea3adf
XiaoHuoZZ <314297421@qq.com>
2020-11-26 15:01:18 +0800

    finish

--- svm/svm.py
+++ svm/svm.py
@@ -15,6 +15,7 @@ from sklearn.pipeline import Pipeline
 import numpy as np
 from sklearn.metrics import confusion_matrix,precision_score,recall_score,f1_score, classification_report
 import joblib
+import time
 
 
 
@@ -25,10 +26,7 @@ def pretreatment():
 
     d_test = pd.read_csv(root_dir+'\\test.csv')
 
-    d_train.dropna(inplace=True)
-    d_test.dropna(inplace=True)
 
-    print(np.isnan(d_train).any())
 
     label_mapping = {'house':0, 'sports':1, 'yule':2, 'news':3, 'business':4, '2008':5, 'health':6, 'women':7, 'it':8, 'auto':9}
     d_train['category'] = d_train['category'].map(label_mapping)
@@ -63,10 +61,6 @@ def training():
     root_dir = 'D:\pythonWorkSpace\Bayes\svm'
     train_articles = joblib.load(root_dir+'/Values/train_articles.pkl')
     train_labels = joblib.load(root_dir+'/Values/train_labels.pkl')
-
-    tr = np.ndarray(train_articles)
-    te = np.ndarray(train_labels)
-
     
     
 
@@ -75,9 +69,10 @@ def training():
                     ('clf',LinearSVC())])
     # 训练
     text_clf.fit(train_articles, train_labels)
-    joblib.dump(text_clf, './Values/text_clf.pkl')
+    joblib.dump(text_clf, root_dir+'./Values/text_clf.pkl')
 
 def predict():
+    start = time.time()
     root_dir = 'D:\pythonWorkSpace\Bayes\svm'
     test_articles = joblib.load(root_dir+'/Values/test_articles.pkl')
     test_labels = joblib.load(root_dir+'/Values/test_labels.pkl')
@@ -89,7 +84,7 @@ def predict():
 
 
     # 测试集标签
-    print("LinearSVC准确率为：",np.mean(predicted==test_labels))
+    # print("LinearSVC准确率为：",np.mean(predicted==test_labels))
 
 
     # cc = confusion_matrix(test_labels, predicted)
@@ -98,30 +93,22 @@ def predict():
     # f1a = f1_score(test_labels, predicted, average=None)
 
     print(classification_report(test_labels, predicted))
+    end = time.time()
+    l_time = end -start
+    print ('the time is :%s' %l_time)
     # classes = np.unique(test_labels).tolist()
     # print(classes)
 
 
 
-pretreatment()
+# pretreatment()
 
 # training()
 
+predict()
 
 
 
 
-
-# for i in range(len(classes)):
-#   print("类别{:8}".format(classes[i]), end=" ")
-#   print("精确率为：{}, 召回率为：{}, F1值为：{}".format(pa[i], ra[i], f1a[i]))
-
-# pat = precision_score(test_labels, predicted, average="micro")
-# rat = recall_score(test_labels, predicted, average="micro")
-# f1at = f1_score(test_labels, predicted, average="micro")
-
-# print(pat)
-# print(rat)
-# print(f1at)
 
 
